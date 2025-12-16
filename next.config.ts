b81e9f1532a7e70aae98c788fc32faaa33fcc833
solankismit <projects.smit@gmail.com>
2025-12-17 00:12:52 +0530

    updated schema

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
 
-// @ts-ignore - Prisma plugin doesn't have types
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
-
-const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...(config.plugins || []), new PrismaPlugin()];
-    }
-    return config;
-  },
-  // Ensure Prisma binaries are included in the build
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
